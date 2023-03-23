c2be940d558a902b68c0051ad3aaec3a508b4d0e
Sutton <suttoncharpentier@gmail.com>
2023-03-22 18:31:07 -0600

    updated and fixed issues with code

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// Assignment code here
 var characters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
 var numbers = "0,1,2,3,4,5,6,7,8,9";
 var specialCharacters = "!,@,#,$,%,^,&,*,(,),-,_,=,+"
 var password = document.getElementById("password");
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
 
  function genPassword() {
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -18,5 +14,11 @@ var generateBtn = document.querySelector("#generate");
   }
         document.getElementById("password").value = password;
  }
-// Add event listener to generate button
+
+ function copyPassword() {
+   var copyText = document.getElementById("password");
+   copyText.select();
+   document.execCommand("copy");  
+ }
+
 generateBtn.addEventListener("click", genPassword());
